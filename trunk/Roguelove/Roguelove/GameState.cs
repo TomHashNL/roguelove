79343c5e29e4f5b8b02e0c3e955964b5806f4545
TomHashNL@gmail.com <TomHashNL@gmail.com@a5616c82-a0f2-3de6-414f-b1c7747b390c>
2012-08-01 15:36:43 +0000

    awesome Wilmer go!

---
+++ trunk/Roguelove/Roguelove/GameState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roguelove
+{
+    public abstract class GameState
+    {
+        /// <summary>
+        /// DO NOT SET
+        /// </summary>
+        public Game1 game;
+
+        public GameState(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public abstract void Update();
+
+        public abstract void Draw();
+    }
+}
